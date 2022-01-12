01faf162e751322f3be13d176e7e118c10b79cc7
Anahi <apv@gmail.com>
2022-01-13 00:10:31 +0100

    mock de tareas y el interface de las mismas

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   }
 
   toggleAddTask(){
-    console.log("toggleAddTask!")
+    console.log("toggleAddTask!");
   }
 
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+{   
+    id: 1,
+    text: "tarea 1",
+    day: "junio 23",
+    reminder: true
+},
+{   
+    id: 2,
+    text: "tarea 2",
+    day: "mayo 25",
+    reminder: true
+},
+{   
+    id: 3,
+    text: "tarea 3",
+    day: "diciembre 12",
+    reminder: true
+}
+]
