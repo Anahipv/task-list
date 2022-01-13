8e3ca9f4691dae3519817604c4111dc47280cb53
Anahi <apv@gmail.com>
2022-01-13 01:03:36 +0100

    lista de items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TasksItemComponent } from './component/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent
   ],
   imports: [
     BrowserModule,
