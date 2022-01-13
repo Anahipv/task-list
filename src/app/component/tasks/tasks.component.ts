f1aca921ef47bb63159c13180421f9fe826741a6
Anahi <apv@gmail.com>
2022-01-13 18:16:19 +0100

    servicio y servidor de tareas listo

--- src/app/component/tasks/tasks.component.ts
+++ src/app/component/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-tasks';
-
 
 @Component({
   selector: 'app-tasks',
@@ -10,11 +9,15 @@ import { TASKS } from 'src/app/mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>(
+    this.tasks = tasks));
   }
 
 }
